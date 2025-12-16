3ed90188a922a508083e412afbe284dc5f0f8a9a
Joicexxh <catce.2024211mtds0024@aluno.ifpi.edu.br>
2025-12-16 12:20:15 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,8 @@
 const API_URL = "https://fullbellyy.onrender.com/api/";
+fetch("/api/cadastro", {
+    method: "POST",
+    body: JSON.stringify(dados),
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
