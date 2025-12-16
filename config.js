1575d1204979e6ccf32ca62467aa150360ae7a90
Joicexxh <catce.2024211mtds0024@aluno.ifpi.edu.br>
2025-12-16 10:27:15 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,7 @@
 // config.js
 const API_URL = "https://fullbellyy.onrender.com/";
+
+fetch(API_URL + "api/foods/")
+  .then(res => res.json())
+  .then(data => console.log("API OK:", data))
+  .catch(err => console.error("API ERRO:", err));
